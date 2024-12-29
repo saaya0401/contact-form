354cb6894695f6b35e0f0db1474e5e170df471be
koba saaya <saaya.koba0401@gmail.com>
2024-12-29 18:27:33 +0900

    contactformの機能追加

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Models\Category;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
     public function index(){
-        return view('index');
+        $categories=Category::all();
+        return view('index', compact('categories'));
     }
-    public function confirm(){
-        return view('confirm');
+    public function confirm(ContactRequest $request){
+        $contact=$request->only(['first_name', 'gender', 'email', 'tel', 'address', 'building', 'category_id', 'detail']);
+        $tel_parts=$request->input('tel');
+        $tel=implode('', $tel_parts);
+        $first_name=$request->input('first_name');
+        $last_name=$request->input('last_name');
+        $full_name=$first_name . ' ' . $last_name;
+        $category=Category::find($contact['category_id']);
+        return view('confirm', compact('contact', 'tel', 'full_name', 'category'));
     }
     public function thanks(){
+        
         return view('thanks');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -18,5 +18,5 @@ Route::middleware('auth')->group(function (){
     Route::get('/admin', [AuthController::class, 'admin']);
 });
 Route::get('/', [ContactController::class, 'index']);
-Route::get('/confirm', [ContactController::class, 'confirm']);
-Route::get('/thanks', [ContactController::class, 'thanks']);
+Route::post('/confirm', [ContactController::class, 'confirm']);
+Route::post('/thanks', [ContactController::class, 'thanks']);

66f4f3047599b28887f3b04ee04960ab3f934301
koba saaya <saaya.koba0401@gmail.com>
2024-12-30 10:10:32 +0900

    お問い合わせフォーム完成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
         return view('index', compact('categories'));
     }
     public function confirm(ContactRequest $request){
-        $contact=$request->only(['first_name', 'gender', 'email', 'tel', 'address', 'building', 'category_id', 'detail']);
+        $contact=$request->only(['first_name', 'last_name', 'gender', 'email', 'tel', 'address', 'building', 'category_id', 'detail']);
         $tel_parts=$request->input('tel');
         $tel=implode('', $tel_parts);
         $first_name=$request->input('first_name');
@@ -23,8 +23,18 @@ class ContactController extends Controller
         $category=Category::find($contact['category_id']);
         return view('confirm', compact('contact', 'tel', 'full_name', 'category'));
     }
-    public function thanks(){
-        
+    public function edit(ContactRequest $request){
+        $contact=$request->only(['first_name', 'last_name', 'email', 'gender', 'tel', 'address', 'building', 'category_id', 'detail']);
+        $categories=Category::all();
+        return view('index', compact('contact', 'categories'));
+    }
+
+    public function thanks(ContactRequest $request){
+        $contact=$request->only(['first_name', 'last_name', 'email', 'gender', 'address', 'building', 'category_id', 'detail']);
+        $tel_parts=$request->input('tel');
+        $tel=implode('', $tel_parts);
+        $contact['tel']=$tel;
+        Contact::create($contact);
         return view('thanks');
     }
 }
--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -14,55 +14,55 @@
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">お名前</th>
                 <td class="confirm-table__content">
-                    <input type="text" value="山田 太郎" class="confirm-table__input" readonly>
-                    <input type="hidden" name="first_name" value="">
-                    <input type="hidden" name="last_name" value="">
+                    <input type="text" value="{{$full_name}}" class="confirm-table__input" readonly>
+                    <input type="hidden" name="first_name" value="{{$contact['first_name']}}">
+                    <input type="hidden" name="last_name" value="{{$contact['last_name']}}">
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">性別</th>
                 <td class="confirm-table__content">
-                    <input type="text" name="gender" value="男性" class="confirm-table__input" readonly>
+                    <input type="text" name="gender" value="{{$contact['gender']}}" class="confirm-table__input" readonly>
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">メールアドレス</th>
                 <td class="confirm-table__content">
-                    <input type="text" name="email" value="test@example.com" class="confirm-table__input" readonly>
+                    <input type="text" name="email" value="{{$contact['email']}}" class="confirm-table__input" readonly>
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">電話番号</th>
                 <td class="confirm-table__content">
-                    <input type="hidden" name="tel[0]" value="" >
-                    <input type="hidden" name="tel[1]" value="" >
-                    <input type="hidden" name="tel[2]" value=""  >
-                    <input type="text"  value="08012345678" class="confirm-table__input" readonly>
+                    <input type="hidden" name="tel[0]" value="{{$contact['tel'][0]}}" >
+                    <input type="hidden" name="tel[1]" value="{{$contact['tel'][1]}}" >
+                    <input type="hidden" name="tel[2]" value="{{$contact['tel'][2]}}"  >
+                    <input type="text"  value="{{$tel}}" class="confirm-table__input" readonly>
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">住所</th>
                 <td class="confirm-table__content">
-                    <input type="text" name="address" value="東京都渋谷区千駄ヶ谷1-2-3" class="confirm-table__input" readonly>
+                    <input type="text" name="address" value="{{$contact['address']}}" class="confirm-table__input" readonly>
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">建物名</th>
                 <td class="confirm-table__content">
-                    <input type="text" name="building" value="千駄ヶ谷マンション101" class="confirm-table__input" readonly>
+                    <input type="text" name="building" value="{{$contact['building']}}" class="confirm-table__input" readonly>
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">お問い合わせの種類</th>
                 <td class="confirm-table__content">
-                    <input type="hidden" name="category_id" value="" >
-                    <input type="text" value="商品の交換について" readonly class="confirm-table__input">
+                    <input type="hidden" name="category_id" value="{{$contact['category_id']}}" >
+                    <input type="text" value="{{$category['content']}}" readonly class="confirm-table__input">
                 </td>
             </tr>
             <tr class="confirm-table__row">
                 <th class="confirm-table__header">お問い合わせ内容</th>
                 <td class="confirm-table__content">
-                    <textarea name="detail" class="confirm-table__textarea" readonly>届いた商品が注文した商品ではありませんでした。商品の取り替えをお願いします</textarea>
+                    <textarea name="detail" class="confirm-table__textarea" readonly>{{$contact['detail']}}</textarea>
                 </td>
             </tr>
         </table>
